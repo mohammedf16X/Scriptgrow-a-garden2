e18426e5ccb64daffce8c4aae465e6b64cbef94e
mohammedf16X <fivehjoftoj366@gmail.com>
2025-07-15 03:35:22 +0300

    Update Grow.lua

--- Grow.lua
+++ Grow.lua
@@ -1,6 +1,6 @@
 local Spawner = loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
 Spawner.Load()
 
-task.wait(1)
+task.wait(3)
 
 loadstring(game:HttpGet("https://pastefy.app/8ZUiKaM6/raw"))()
